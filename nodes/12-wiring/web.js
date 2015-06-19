712f825c9ca8e70c7650596dd84590c88f94018f
Alexandru Radovici <alexandru@wylidrin.com>
2015-06-19 23:24:45 +0000

    websrever

--- nodes/12-wiring/web.js
+++ nodes/12-wiring/web.js
@@ -169,7 +169,8 @@ module.exports = function(RED) {
             app.use (bodyparser.json());
             app.use (bodyparser.urlencoded({ extended:true }));
             app.use ('static', express.static(process.cwd()+'/static'));
-            var server = app.listen (that.port);
+            var server = require ('http').Server (app);
+            server.listen (that.port);
             socketio.attach (server);
             socketio.on ('connection', function (connection)
             {
